9c5233c0c836c60cf27e799dac1ac3128c37bc28
Wilmilcard <leonjuandavid@hotmail.com>
2021-09-17 14:01:34 -0500

    Algunos Seeder

--- GameStore.Domain/Seeders/ModelBuilderExtensions.cs
+++ GameStore.Domain/Seeders/ModelBuilderExtensions.cs
@@ -61,6 +61,230 @@ namespace GameStore.Domain.Seeders
                     Nombre = "Mario Bross",
                     CreatedAt = DateTime.Now,
                     CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 2,
+                    Nombre = "Tommy Vercetti",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 3,
+                    Nombre = "Altaïr Ibn-La'Ahad",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 4,
+                    Nombre = "Natan Drake",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 5,
+                    Nombre = "Crash Bandicoot",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 6,
+                    Nombre = "Samus Aran",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 7,
+                    Nombre = "John-117",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 8,
+                    Nombre = "Aiden Perce",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 9,
+                    Nombre = "Carl Jhonson",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 10,
+                    Nombre = "Red",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 11,
+                    Nombre = "Crazy Dave",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 12,
+                    Nombre = "Spyro",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 13,
+                    Nombre = "Marcus Fenix",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 14,
+                    Nombre = "Vass",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 15,
+                    Nombre = "Gordon Freeman",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 16,
+                    Nombre = "Joel",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 17,
+                    Nombre = "Jill Valentine",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 18,
+                    Nombre = "Zelda",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 19,
+                    Nombre = "Link",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id =20,
+                    Nombre = "Glados",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 21,
+                    Nombre = "Meet Boy",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 22,
+                    Nombre = "Geralt de Rivia",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 23,
+                    Nombre = "Steve",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 24,
+                    Nombre = "Ellie",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 25,
+                    Nombre = "Faith",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 26,
+                    Nombre = "Bayonetta",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 27,
+                    Nombre = "Tracer",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 28,
+                    Nombre = "Pacman",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 29,
+                    Nombre = "Chris Redfield",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 30,
+                    Nombre = "Leon S. Kennedy",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 31,
+                    Nombre = "Agente 47",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 32,
+                    Nombre = "Haytham Kenway",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Protagonista
+                {
+                    Id = 33,
+                    Nombre = "Lara Croft",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
                 }
             );
 
@@ -71,6 +295,62 @@ namespace GameStore.Domain.Seeders
                     Nombre = "Microsoft",
                     CreatedAt = DateTime.Now,
                     CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 2,
+                    Nombre = "Sony",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 3,
+                    Nombre = "EA",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 4,
+                    Nombre = "Ubisoft",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 5,
+                    Nombre = "Rovio",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 6,
+                    Nombre = "Activision",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 7,
+                    Nombre = "Nintendo",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 8,
+                    Nombre = "Rockstar",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Marca
+                {
+                    Id = 9,
+                    Nombre = "CD Project Red",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
                 }
             );
 
@@ -79,6 +359,111 @@ namespace GameStore.Domain.Seeders
                 {
                     Id = 1,
                     Nombre = "PC",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 2,
+                    Nombre = "Xbox",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 3,
+                    Nombre = "Xbox 360",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 4,
+                    Nombre = "Xbox ONE",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 5,
+                    Nombre = "Xbox X",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 6,
+                    Nombre = "PSP Vita",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 7,
+                    Nombre = "PlayStation",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 8,
+                    Nombre = "PlayStation 2",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 9,
+                    Nombre = "PlayStation 3",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 10,
+                    Nombre = "PlayStation 4",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 11,
+                    Nombre = "PlayStation 5",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 12,
+                    Nombre = "Nintendo 64",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 13,
+                    Nombre = "Nintendo DS",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 14,
+                    Nombre = "Nintendo Switch",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 15,
+                    Nombre = "Android",
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "JDLB"
+                },
+                new Plataforma
+                {
+                    Id = 16,
+                    Nombre = "IOS",
                     CreatedAt = DateTime.Now,
                     CreatedBy = "JDLB"
                 }
