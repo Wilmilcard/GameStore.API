27ed64c673a3404d559ab6a94ec13eb00643f382
Wilmilcard <leonjuandavid@hotmail.com>
2021-09-20 19:37:19 -0500

    Configuracion para el puerto 5001 - lo dejo documentado pero no lo uso

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GameStore.API
@@ -14,7 +16,22 @@ namespace GameStore.API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //BuildWebHost(args).Run();
         }
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        // WebHost.CreateDefaultBuilder(args)
+        //.UseStartup<Startup>()
+        //.UseKestrel(options =>
+        //{
+        //    options.Limits.MaxConcurrentConnections = 1000;
+        //    options.Limits.MaxConcurrentUpgradedConnections = 1000;
+        //    options.Limits.MaxRequestBodySize = 30000000;
+        //    options.Limits.KeepAliveTimeout = new TimeSpan(2, 0, 0);
+        //    options.Listen(IPAddress.Any, 5001);
+        //})
+        //.Build();
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
